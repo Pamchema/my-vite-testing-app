2995a12de4bc76ed3df128ca0e26c09bbd6f7378
Pamchema <urechique@gmail.com>
2023-11-20 21:32:29 +0100

    edited the vite.config .js

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default {
+
+// }
 export default defineConfig({
   plugins: [react()],
+      base: '/'
 })
